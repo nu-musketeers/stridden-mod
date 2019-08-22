58b365e72ece35d6462372a8708d91ad79afb392
Stridden <Stridden@users.noreply.github.com>
2019-08-22 09:32:18 -0500

    Massive restructure
    * Massive restructure
    * Fix us lang file

---
+++ src/main/java/stridden/enrich/init/ModTileEntities.java
@@ -0,0 +1,34 @@
+/**
+    Copyright (C) 2017 by jabelar
+
+    This file is part of jabelar's Minecraft Forge modding examples; as such,
+    you can redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software Foundation,
+    either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    For a copy of the GNU General Public License see <http://www.gnu.org/licenses/>.
+*/
+package stridden.enrich.init;
+
+import stridden.enrich.tileentities.TileEntityCompactor;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModTileEntities
+{
+    /**
+     * Registers tile entities.
+     */
+    public static void registerTileEntities()
+    {
+        // DEBUG
+        System.out.println("Registering tile entities");
+        GameRegistry.registerTileEntity(TileEntityCompactor.class, "tileEntityCompactor");
+    }
+
+}
